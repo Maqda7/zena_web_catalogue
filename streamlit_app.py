79f890237091c7d0394ac3cb597f1cfe1088be75
Maqda7 <maqda7@gmail.com>
2023-08-30 16:41:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,29 @@
 import streamlit 
 import snowflake.connector
+import pandas
 
-streamlit.title('Web App Catalogue') 
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# connect to snowflake 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+
+# run a snowflake query and put it all in a var called my_catalog 
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+df = pandas.DataFrame(my_catalog)
+color_list = df[0].values.tolist()
+
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+
+streamlit.image( df2[0], width=400, caption= product_caption )
+
+streamlit.write('Price: ', df2[1])
+streamlit.write('Sizes Available: ',df2[2]) 
+streamlit.write(df2[3])
